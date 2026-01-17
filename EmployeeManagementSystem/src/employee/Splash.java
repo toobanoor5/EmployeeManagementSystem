324462ebcb56b82f8b5e62f60204962a6fab70c5
toobanoor5 <toobanoor320gb@gmail.com>
2026-01-17 21:48:17 +0500

    4th commit

--- EmployeeManagementSystem/src/employee/Splash.java
+++ EmployeeManagementSystem/src/employee/Splash.java
@@ -1,1 +1,36 @@
-package employee;import javax.swing.*;import java.awt.*;public class Splash extends JFrame {	private static final long serialVersionUID = 1L;    Splash(){        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/front.gif"));        Image i2 = i1.getImage().getScaledInstance(1170,650, Image.SCALE_DEFAULT);        ImageIcon i3 = new ImageIcon(i2);        JLabel image = new JLabel(i3);        image.setBounds(0,0,1170,650);        add(image);        setSize(1170,650);        setLocation(200,50);        setLayout(null);        setVisible(true);        Timer timer = new Timer(5000, e -> {            setVisible(false);            new Login();         });        timer.setRepeats(false);        timer.start();    }    public static void main(String[] args) {        new Splash();    }}
+package employee;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Splash extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+
+    Splash(){
+
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/front.gif"));
+        Image i2 = i1.getImage().getScaledInstance(1170,650, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0,0,1170,650);
+        add(image);
+
+
+        setSize(1170,650);
+        setLocation(200,50);
+        setLayout(null);
+        setVisible(true);
+
+        Timer timer = new Timer(5000, e -> {
+            setVisible(false);
+            new Login(); 
+        });
+        timer.setRepeats(false);
+        timer.start();
+    }
+
+    public static void main(String[] args) {
+        new Splash();
+    }
+}
--- EmployeeManagementSystem/src/employee/conn.java
+++ EmployeeManagementSystem/src/employee/conn.java
@@ -1,1 +1,22 @@
-package employee;import java.sql.*;public class conn {    Connection connection;    Statement statement;    public conn(){        try{            Class.forName("com.mysql.cj.jdbc.Driver");            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee","root","#Noorfatima5");            statement = connection.createStatement();        }catch (Exception e){            e.printStackTrace();        }    }}
+package employee;
+import java.sql.*;
+
+
+
+public class conn {
+
+    Connection connection;
+    Statement statement;
+
+    public conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/employee","root","#Noorfatima5");
+            statement = connection.createStatement();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
